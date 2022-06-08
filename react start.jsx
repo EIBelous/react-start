f08b25445362a59fa7314eb872be7fff83f3c0cb
EIBelous <evgen.i.bilous@gmail.com>
2022-06-08 06:08:09 +0300

    react classes

--- react start.jsx
+++ react start.jsx
@@ -1,29 +1,28 @@
-function Button(props) {
-	
-    const handleClick=()=>(props.clicker(props.increment))
-       return (<button onClick={handleClick}>
-       +{props.increment}
-     </button>)
-   }
-   
-   function Display(props){
-     return(
-     <div>{props.message}</div>);
-   }
-   
-   function App(){
-     const [counter, setCounter] = useState(22);
-     const handleClick=(incrementValue)=>setCounter(counter+incrementValue)
-     return(
-         <div>
-     <Button clicker={handleClick} increment={1}/>
-         <Button clicker={handleClick} increment={2}/>
-         <Button clicker={handleClick} increment={5}/>
-         <Button clicker={handleClick} increment={10}/>
-         <Display message={counter} />
-         </div>)
-   }
-   ReactDOM.render(
-     <App />, 
-     document.getElementById('mountNode'),
+class Card extends React.Component{
+  render(){
+    return (
+    <div className="github-profile">
+     <img src="https://plasehold.it/75"/>
+       <div className="info">
+          <div className="name"> name here </div>
+          <div className="company"> company here </div>
+        </div>
+    </div>
+    );
+  }
+}
+class App extends React.Component{
+  render(){
+  return ( <div className="header">{this.props.title}
+     <Card />
+    </div>
    );
+  }
+}
+
+
+
+ReactDOM.render(
+	<App title="The GitHub Cards App" />,
+  mountNode,
+);
