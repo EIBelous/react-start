6cff8a6a6a698cd36f8e512c6c7196e95a878039
EIBelous <evgen.i.bilous@gmail.com>
2022-06-15 06:34:24 +0300

    taking input from the user

--- react start.jsx
+++ react start.jsx
@@ -26,10 +26,22 @@ render() {
 }
 
 class Form extends React.Component{
+  state={userName:''}
+handleSubmit=(event)=>{
+  event.preventDefault();
+  console.log(
+  this.state.userName)
+  
+}
 render(){
   return(
-  <form action="">
-      <input type="text" placeholder="Git add" />
+  <form onSubmit={this.handleSubmit}>
+      <input type="text"
+        placeholder="Git add User" 
+        value={this.state.userName}
+        onChange={event=>this.setState({userName:event.target.value})}
+        required />
+      <button>Add card</button>
       </form>)
 }
 }
