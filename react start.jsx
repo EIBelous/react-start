7c7ecc28373543e4b6f87350a5b6968c6bf81a1e
EIBelous <evgen.i.bilous@gmail.com>
2022-06-15 06:10:43 +0300

    initialising the state obj

--- react start.jsx
+++ react start.jsx
@@ -1,20 +1,18 @@
-
-	const testData = [
-    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
 ];
 
-const CardList=(props)=>(
+const CardList = (props) => (
 <div>
-{testData.map(profile=><Card {...profile}/>)}
-
-</div>)
-
+  {props.profiles.map(profile => <Card {...profile}/>)}
+</div>
+);
 
 class Card extends React.Component {
 render() {
-  const profile=this.props
+  const profile = this.props;
   return (
     <div className="github-profile">
       <img src={profile.avatar_url} />
@@ -27,12 +25,28 @@ render() {
 }
 }
 
+class Form extends React.Component{
+render(){
+  return(
+  <form action="">
+      <input type="text" placeholder="Git add" />
+      </form>)
+}
+}
+
 class App extends React.Component {
+constructor(props){
+  super(props)
+  this.state={
+    profiles:testData,
+  }
+}
 render() {
   return (
     <div>
       <div className="header">{this.props.title}</div>
-      <CardList />
+      <Form />
+      <CardList profiles={this.state.profiles} />
     </div>
   );
 }	
