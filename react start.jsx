742f43469b18048a65d5731d5fbae6a1134a5f5c
EIBelous <evgen.i.bilous@gmail.com>
2022-06-23 06:28:48 +0300

    function state=>state

--- react start.jsx
+++ react start.jsx
@@ -1,19 +1,39 @@
-// STAR MATCH - V2
-const StarsDisplay=props=>(
+// STAR MATCH - V3
+
+const StarsDisplay = props => (
   <>
- {utils.range(1, props.count).map(starId =>
-          	<div key={starId} className="star" />
-)}
-    </>
+    {utils.range(1, props.count).map(starId => (
+      <div key={starId} className="star" />
+    ))}
+  </>
 );
-                            
-const PlayNumber = (props)=>(
-	<button className="number" onClick={()=>console.log( props.number)}>
+
+const PlayNumber = props => (
+	<button 
+    className="number" 
+    style={{backgroundColor:colors[props.status]}}
+    onClick={() => console.log('Num', props.number)}>
     {props.number}
-  </button>)
+  </button>
+);
 
 const StarMatch = () => {
 	const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums]=useState(utils.range(1,9))
+const [candidateNums,setCandidateNums]=useState([])
+  
+  const cadidatesAreWrong=utils.sum(candidateNums)>stars
+        
+  const numberStatus=(number)=>{
+  if (!availableNums.includes(number)){
+    return 'used'
+  }  
+    if(candidateNums.includes(number)){
+      return cadidatesAreWrong ? 'wrong':'candidate';
+    }
+    return 'available'
+  } 
+  
   return (
     <div className="game">
       <div className="help">
@@ -21,12 +41,14 @@ const StarMatch = () => {
       </div>
       <div className="body">
         <div className="left">
-         < StarsDisplay  count={stars}/>
-          
+					<StarsDisplay count={stars}/>
         </div>
         <div className="right">
         	{utils.range(1, 9).map(number =>
-          < PlayNumber key={number}  number={number}/>
+          	<PlayNumber 
+              key={number} 
+              status={numberStatus(number)}
+              number={number}/>
           )}
         </div>
       </div>
